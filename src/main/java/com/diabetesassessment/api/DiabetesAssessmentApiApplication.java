2867981b471e479996e994db49fdd4fb844f5ea9
davidshin0302 <david.shin0302@gmail.com>
2025-01-02 11:36:54 -0500

    first commit.

---
+++ src/main/java/com/diabetesassessment/api/DiabetesAssessmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.diabetesassessment.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesAssessmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesAssessmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diabetesassessment/api/DiabetesAssessmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diabetesassessment.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesAssessmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
